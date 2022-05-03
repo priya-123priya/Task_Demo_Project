a21f49fe3cf848bde1f16ebfd440e88030cc531f
Your Name <you@example.com>
2022-05-03 15:21:11 +0530

    first commit

---
+++ src/main/java/Maven_Basics/App.java
@@ -0,0 +1,12 @@
+package Maven_Basics;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
